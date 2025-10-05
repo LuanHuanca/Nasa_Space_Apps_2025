771d1542df3c6eeab7d12f7cddbebf64286b8d60
Alejandro <cra2028252@est.univalle.edu>
2025-10-05 01:30:17 -0400

    coreccion de datos para modelo de api

--- Api/main.py
+++ Api/main.py
@@ -18,9 +18,16 @@ KOI_URL = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+kepoi
 CANDIDATE_URL = (
     "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?"
     "table=cumulative&where=koi_disposition%20like%20%27CANDIDATE%27"
-    "&where=koi_period%3E300,koi_prad%3C2&order=koi_period"
-    "&select=kepid,kepoi_name,koi_period,koi_time0bk,koi_steff,"
-    "koi_impact,koi_duration,ra,dec&format=json"
+    "&where=koi_period%3E300,koi_prad%3C2&order=koi_period&format=json"
+    "&select=kepid,kepoi_name,koi_score,koi_fpflag_nt,koi_fpflag_ss,"
+    "koi_fpflag_co,koi_fpflag_ec,koi_period,koi_period_err1,koi_period_err2,"
+    "koi_time0bk,koi_time0bk_err1,koi_time0bk_err2,koi_impact,koi_impact_err1,"
+    "koi_impact_err2,koi_duration,koi_duration_err1,koi_duration_err2,"
+    "koi_depth,koi_depth_err1,koi_depth_err2,koi_prad,koi_prad_err1,"
+    "koi_prad_err2,koi_teq,koi_teq_err1,koi_teq_err2,koi_insol,koi_insol_err1,"
+    "koi_insol_err2,koi_model_snr,koi_tce_plnt_num,koi_tce_delivname,"
+    "koi_steff,koi_steff_err1,koi_steff_err2,koi_slogg,koi_slogg_err1,"
+    "koi_slogg_err2,koi_srad,koi_srad_err1,koi_srad_err2,ra,dec,ra_str,dec_str,koi_kepmag"
 )
 
 
